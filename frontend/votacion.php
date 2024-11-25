77a18b2d4cc31a0e33288acf3bb56b3ef209506d
MJSDaw <mjsdaw@gmail.com>
2024-11-25 12:26:54 +0000

    "terminado" el css

--- frontend/votacion.php
+++ frontend/votacion.php
@@ -11,30 +11,30 @@
 </head>
 <body>
     <header>
-        <div id="logo">
-            <img src="../media/logo-ies-fernando-sagaseta.png" alt="logo-ies-fernando-sagaseta" width="150px">
+        <div id="logo"><img src="../media/logo-ies-fernando-sagaseta.png" alt="logo-ies-fernando-sagaseta">
         </div>
         <div id="titulo-estado">
             <h1>CONCURSO DE CARTELERIA</h1>
             <h2>ESTADO: ABIERTO</h2>
         </div>
     </header>
-    <main id="form-part">
-        <div id="cartel">
-            <h3>ANÓNIMO</h3>
-            <img src="" alt="Cartel  de ANONIMO">
+    <main id="form_part">
+        <div id="requisitos">
+            <div id="cartel">
+                <h3>Título</h3>
+                <img src="../media/cartel_prueba.jpeg" alt="Cartel  de ANONIMO">
+            </div>
         </div>
         <div id="formulario">
-            <h3>FORMULARIO PARA VOTAR</h3>
             <form action="" method="post">
-                <label for="cial">CIAL: </label>
-                <input type="text" id="CIAL" required><br>
-                <label for="pin">PIN: </label>
-                <input type="text" id="PIN" required><br>
-                <input type="submit" value="Votar">
+                <h3>FORMULARIO DE INSCRIPCIÓN</h3>
+                <div><label for="cial">CIAL: </label>
+                <input type="text" name="cial" id="cial" required></div>
+                <div><label for="pin">PIN: </label>
+                <input type="text" name="pin" id="pin" required></div>
+                <input type="submit" value="Subir" id="subir">
             </form>
         </div>
-        <a href="index.php">Volver a la página principal</a>
     </main>
     <footer>
         <button id="participar" onclick="volver()">Volver a la página principal</button>
@@ -47,3 +47,8 @@
     </script>
 </body>
 </html>
+
+<!-- <div id="cartel">
+            <h3>ANÓNIMO</h3>
+            <img src="" alt="Cartel  de ANONIMO">
+        </div> -->
